283e8f350bad0ce459785e5474bb3ebfa6c4b87f
AlbertoZamarchi <albertozamarchi@hotmail.com>
2024-09-19 22:10:32 -0300

    aula_19_09_2024

--- mater-play-frontend/src/App.tsx
+++ mater-play-frontend/src/App.tsx
@@ -1,11 +1,26 @@
+import Footer from "./app/components/Footer"
 import Header from "./app/components/Header"
+import HighLightSection from "./app/components/HighLightSection"
+import Section from "./app/components/Section"
+
 
 function App() {
 
   return (
     <div className="wrapper">
-      <Header />
-    </div> 
+      <Header/>
+      <main
+        style={{
+          marginTop: '8rem'
+        }}
+      >
+        <HighLightSection />
+        <Section title="Recomendados para você"/>
+        <Section title="Para toda a familia"/>
+      </main>
+      <Footer/>
+    </div>
+
   )
 }
 
