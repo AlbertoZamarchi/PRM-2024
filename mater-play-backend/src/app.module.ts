800bdd3a6113f199197acced58a06c1e5b6d00f2
AlbertoZamarchi <albertozamarchi@hotmail.com>
2024-10-10 22:13:28 -0300

    aula_10-10-2024

---
+++ mater-play-backend/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from './entities/movie-entity';
+import { MovieController } from './controllers/movie-controller';
+import { MovieService } from './services/movie-service';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [Movie],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Movie]),
+  ],
+  
+  controllers: [MovieController],
+  providers: [MovieService],
+})
+export class AppModule {}
